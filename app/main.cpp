8c436e9397d0f4882f35e85d8f7f2473840a1554
sshazly <sshazly@ncsu.edu>
2017-10-18 18:26:28 -0500

    Completed generalized forward kinematics and csv file reading

--- app/main.cpp
+++ app/main.cpp
@@ -1,5 +1,6 @@
-#include <iostream>
+#include <sstream>
 #include <ROBOTlib.hpp>
+using namespace std;
 
 int main()
 {
@@ -10,21 +11,51 @@ int main()
     robby.leftMotor.setKB(0.1);
     robby.rightMotor.setKB(0.1);
     robby.chassis.setHeight(1);
-    robby.chassis.setWidth(1);
-    robby.chassis.setLength(1);
+    robby.chassis.setWidth(5);
+    robby.chassis.setLength(3);
 
-    struct path p=robby.importPath();
+	std::vector <vector<string>>data;
+	std::string filenameNautilus=("/home/viki/temp/robotMotionController/app/plannedPathText.txt");
+	std::ifstream fileIN;
+
+
+	if(fileIN.is_open())
+	{
+		std::cout << "ERROR: File Open" << '\n';
+		fileIN.close();
+		fileIN.open(filenameNautilus.c_str());
+	}else{
+		fileIN.open(filenameNautilus.c_str());
+	};
+
+    std::string x,y,T,t;
+	path p;
+
+	while(fileIN.good()){
+		getline(fileIN,x,',');
+		getline(fileIN,y,',');
+		getline(fileIN,T,',');
+		getline(fileIN,t,'\n');
+
+		p.x.push_back(strtod(x.c_str(),NULL));
+		p.y.push_back(strtod(y.c_str(),NULL));
+		p.theta.push_back(strtod(T.c_str(),NULL));
+		p.time.push_back(strtod(t.c_str(),NULL));
+	}
+	fileIN.close();
     std::vector<double> speedLeftVec;
     std::vector<double> speedRightVec;
-    for(int i=0;i<p.x.size();i++){
+    for(unsigned i=0;i<p.x.size()-2;i++){
     	double dx=p.x[i+1]-p.x[i];
     	double dy=p.y[i+1]-p.y[i];
     	double dT=p.theta[i+1]-p.theta[i];
     	double dt=p.time[i+1]-p.time[i];
     	double T=p.theta[i];
+    	double x=p.x[i];
+    	double y=p.y[i];
 
-    	struct angularVelocity aV = robby.forwardKinematics(dx,dy,dT,T,dt);
-    	cout << "Left Wheel Velocity " << aV.left << "  :  Right Wheel Velocity" << aV.right << "\n";
+    	struct angularVelocity aV = robby.forwardKinematics(dx,dy,dT,x,y,T,dt);
+    	cout << "Left Wheel Velocity " << round(aV.left) << "  :  Right Wheel Velocity " << round(aV.right) << "\n";
     };
 
     return 0;
--- include/ROBOTlib.hpp
+++ include/ROBOTlib.hpp
@@ -27,9 +27,7 @@
 #include <STRUCTURElib.hpp>
 #include <fstream>
 #include <string>
-#include <sstream>
 #include <vector>
-#include <iomanip>
 
 
 using namespace std;
@@ -38,10 +36,9 @@ using namespace std;
 class ROBOT {
 public:
 	struct angularVelocity forwardKinematics(double dx, double dy, double dT,
-			double T, double dt);
+			double x, double y, double T, double dt);
 	std::vector<double> rot2D(double theta, std::vector<double> vec);
 	void simulatePath();
-	struct path importPath();
 	int sign(double a);
 	WHEEL leftWheel;
 	WHEEL rightWheel;
@@ -59,33 +56,63 @@ private:
 
 /// Determine the left right Motor speeds to reach the next target point
 struct angularVelocity ROBOT::forwardKinematics(double dx, double dy, double dT,
-		double T, double dt) {
+		double x, double y, double T, double dt) {
 	/// Gather Robot Parameters (wheel radius, chassis width)
 	double w = chassis.getWidth();
 	double rL = leftWheel.getRadius();
 	double rR = rightWheel.getRadius();
-	/// Create position vector for where the robot wants to go
-	std::vector<double> pos;
-	pos[0] = dx;
-	pos[1] = dy;
+	// Position of wheels relative to frame
+	std::vector <double> pL={-w/2,0};
+	std::vector <double> pR={w/2,0};
 
-	/// Find the projection of the instantaneous velocity on the chassis frame
-	std::vector<double> posrel = rot2D(T, pos);
+	/// Create position vector for where the robot wants to go and where it is
+	std::vector<double> pos_t0 = {x,y};
+	std::vector<double> pos_t1 = {x+dx,y+dy};
 
-	/// Find the
-	double d = sqrt(pow(dx, 2.0) + pow(dy, 2.0));
+	/// Find the position of L/R wheels relative to reference frame at t and t+dt
+	/// Rotation and Translation
+	std::vector<double> dL_t0 = rot2D(T, pL);
+	dL_t0[0]+=pos_t0[0]; // Add x component
+	dL_t0[1]+=pos_t0[1]; // Add y component
+	std::vector<double> dR_t0 = rot2D(T, pR);
+	dR_t0[0]+=pos_t0[0]; // Add x component
+	dR_t0[1]+=pos_t0[1]; // Add y component
+	std::vector<double> dL_t1 = rot2D(T+dT, pL);
+	dL_t1[0]+=pos_t1[0]; // Add x component
+	dL_t1[1]+=pos_t1[1]; // Add y component
+	std::vector<double> dR_t1 = rot2D(T+dT, pR);
+	dR_t1[0]+=pos_t1[0]; // Add x component
+	dR_t1[1]+=pos_t1[1]; // Add y component
+	/// Find the difference between the wheels position
+	/// relative to the reference frame at t and t+dt
+	std::vector<double> dLv = {0,0};
+	dLv[0]= dL_t1[0]-dL_t0[0]; // x-difference relative to reference frame
+	dLv[1]= dL_t1[1]-dL_t0[1]; // y-difference relative to reference frame
+	std::vector<double> dRv = {0,0};
+	dRv[0]= dR_t1[0]-dR_t0[0];  // x-difference relative to reference frame
+	dRv[1]= dR_t1[1]-dR_t0[1];  // y-difference relative to reference frame
+
+	/// Find the new wheel position relative to the old rigid body frame
+	/// this will be used in the directional component for angular velocity
+
+	std::vector<double> dL_t1_rel={dL_t1[0],dL_t1[1]}; // Next left wheel position relative to reference frame
+	dL_t1_rel[0]+=-x; // Translate left wheel from old position rigid body frame to reference frame
+	dL_t1_rel[1]+=-y;
+	std::vector<double> dR_t1_rel={dR_t1[0],dR_t1[1]}; // Next right wheel position relative to reference frame
+	dR_t1_rel[0]+=-x; // Translate right wheel from old position rigid body frame to reference frame
+	dR_t1_rel[1]+=-y;
+
+	/// Rotate vector to be relative to the rigid body frame
+	std::vector<double> relL=rot2D(-T,dL_t1_rel);
+	std::vector<double> relR=rot2D(-T,dR_t1_rel);
+
+	/// Find the norm distance times the magnitude
+	double dL = sqrt(pow(dLv[0], 2.0) + pow(dLv[1], 2.0));
+	double dR = sqrt(pow(dRv[0], 2.0) + pow(dRv[1], 2.0));
+
 	angularVelocity aV;
-	int fD = sign(posrel[1]);
-	if (dT != 0) {
-		double curv = d / 2 / (sin(dT / 2));
-		double c1 = curv * fD + w / 2;
-		double c2 = curv * fD - w / 2;
-		aV.left = c1 * dT / dt / rL;
-		aV.right = c2 * dT / dt / rR;
-	} else {
-		aV.left = d / dt / rL;
-		aV.right = d / dt / rR;
-	};
+		aV.left = dL /dt/rL * sign(relL[1]);
+		aV.right = dR / dt/ rR * sign(relR[1]);
 
 	return (aV);
 }
@@ -95,30 +122,9 @@ void ROBOT::simulatePath() {
 	//GNUplot gnu;
 }
 
-/// import path planner file
-struct path ROBOT::importPath() {
-
-	vector <vector<string>>data;
-	ifstream infile("plannedPathText.txt");
-
-    std::string str;
-
-	path p;
-	p.x[0] = -2.5;
-	p.y[0] = 0;
-	p.theta[0] = 0;
-	p.time[0] = 1;
-
-	p.x[1] = -2.3;
-	p.y[1] = -1.55;
-	p.theta[1] = .124;
-	p.time[1] = 1.55;
-
-	return p;
-}
 
 std::vector<double> ROBOT::rot2D(double theta, std::vector<double> vec) {
-	std::vector<double> vecR;
+	std::vector<double> vecR = {0,0};
 	vecR[0] = cos(theta) * vec[0] - sin(theta) * vec[1];
 	vecR[1] = sin(theta) * vec[0] + cos(theta) * vec[1];
 	return (vecR);
